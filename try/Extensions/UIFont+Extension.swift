52b5d299a3bd405bfd53045341d294d73c168f3b
Natalia Sinitsyna <ns@numbuster.com>
2020-10-29 17:25:47 +0300

    Added UIFont+extension, changed the String html func

---
+++ try/Extensions/UIFont+Extension.swift
@@ -0,0 +1,132 @@
+//
+//  UIFont+Extension.swift
+//  try
+//
+//  Created by Natali on 29.10.2020.
+//  Copyright © 2020 Redmadrobot. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIFont {
+    
+    // MARK: 10 - 14
+    
+    class var text_Regular_10: UIFont {
+        return UIFont.systemFont(ofSize: 10.0, weight: .regular)
+    }
+    
+    class var text_Medium_10: UIFont {
+        return UIFont.systemFont(ofSize: 10.0, weight: .medium)
+    }
+    
+    class var text_Bold_10: UIFont {
+        return UIFont.systemFont(ofSize: 10.0, weight: .bold)
+    }
+    
+    class var  text_Regular_11: UIFont {
+        return UIFont.systemFont(ofSize: 11.0, weight: .regular)
+    }
+    
+    class var text_Regular_12: UIFont {
+        return UIFont.systemFont(ofSize: 12.0, weight: .regular)
+    }
+    
+    class var text_Bold_12: UIFont {
+        return UIFont.systemFont(ofSize: 12.0, weight: .bold)
+    }
+    
+    class var  text_Regular_13: UIFont {
+        return UIFont.systemFont(ofSize: 13.0, weight: .regular)
+    }
+
+    class var text_Bold_14: UIFont {
+        return UIFont.systemFont(ofSize: 14.0, weight: .bold)
+    }
+    
+    class var text_Regular_14: UIFont {
+        return UIFont.systemFont(ofSize: 14.0, weight: .regular)
+    }
+    
+    class var text_Medium_14: UIFont {
+        return UIFont.systemFont(ofSize: 14.0, weight: .medium)
+    }
+    
+    class var text_KabelCTT_Medium_14: UIFont {
+        return UIFont(name: "KabelCTT_Medium", size: 14.0) ?? UIFont.systemFont(ofSize: 14.0, weight: .medium)
+    }
+    
+    // MARK: 15 - 17
+    
+    class var text_Bold_15: UIFont {
+        return UIFont.systemFont(ofSize: 15.0, weight: .bold)
+    }
+    
+    class var text_Bold_16: UIFont {
+        return UIFont.systemFont(ofSize: 16.0, weight: .bold)
+    }
+    
+    class var text_Medium_16: UIFont {
+        return UIFont.systemFont(ofSize: 16.0, weight: .medium)
+    }
+    
+    class var text_Regular_16: UIFont {
+        return UIFont.systemFont(ofSize: 16.0, weight: .regular)
+    }
+    
+    class var text_Regular_17: UIFont {
+        return UIFont.systemFont(ofSize: 17.0, weight: .regular)
+    }
+    
+    class var text_Regular_18: UIFont {
+        return UIFont.systemFont(ofSize: 18.0, weight: .regular)
+    }
+    
+    class var text_Semibold_17: UIFont {
+        return UIFont.systemFont(ofSize: 17.0, weight: .semibold)
+    }
+    
+    class var text_Bold_18: UIFont {
+        return UIFont.systemFont(ofSize: 18.0, weight: .bold)
+    }
+    
+    class var text_Thin_19: UIFont {
+        return UIFont.systemFont(ofSize: 19.0, weight: .thin)
+    }
+    
+    // MARK: 20 - 28
+    
+    class var text_Regular_20: UIFont {
+        return UIFont.systemFont(ofSize: 20.0, weight: .regular)
+    }
+    
+    class var text_Semibold_20: UIFont {
+        return UIFont.systemFont(ofSize: 20.0, weight: .semibold)
+    }
+    
+    class var text_Medium_20: UIFont {
+        return UIFont.systemFont(ofSize: 20.0, weight: .medium)
+    }
+
+    class var text_Bold_24: UIFont {
+        return UIFont.systemFont(ofSize: 24.0, weight: .bold)
+    }
+    
+    class var text_Bold_28: UIFont {
+        return UIFont.systemFont(ofSize: 28.0, weight: .bold)
+    }
+    
+    class var text_Bold_30: UIFont {
+        return UIFont.systemFont(ofSize: 30.0, weight: .bold)
+    }
+    
+    class var text_Bold_34: UIFont {
+        return UIFont.systemFont(ofSize: 34.0, weight: .bold)
+    }
+    
+    // MARK: Super Large: 28+
+    class var text_Regular_32: UIFont {
+        return UIFont.systemFont(ofSize: 32.0, weight: .regular)
+    }
+}
